901c4762a2fbf4b5187fd9ae28751a582927fafd
Hidro Le <hidro@forixwebdesign.com>
2023-10-09 14:39:37 +0700

    Upload module

---
+++ Preference/ProcessCronQueueObserver.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Hidro\NewrelicCronjob\Preference;
+
+class ProcessCronQueueObserver extends \Magento\Cron\Observer\ProcessCronQueueObserver
+{
+    protected function _runJob($scheduledTime, $currentTime, $jobConfig, $schedule, $groupId)
+    {
+        if (extension_loaded('newrelic')) {
+            newrelic_end_transaction();
+            newrelic_background_job();
+            newrelic_set_appname(ini_get("newrelic.appname"), '', true);
+            newrelic_name_transaction("/cron/run/" . $jobCode);
+        }
+
+        parent::_runJob($scheduledTime, $currentTime, $jobConfig, $schedule, $groupId);
+    }
+}
