e1bf5a6751c73d2964db99568f5d7f2f077c857c
Hidro Le <hidro@forixwebdesign.com>
2023-10-10 17:11:50 +0700

    Adding job code

--- Preference/ProcessCronQueueObserver.php
+++ Preference/ProcessCronQueueObserver.php
@@ -10,7 +10,7 @@ class ProcessCronQueueObserver extends \Magento\Cron\Observer\ProcessCronQueueOb
             newrelic_end_transaction();
             newrelic_background_job();
             newrelic_set_appname(ini_get("newrelic.appname"), '', true);
-            newrelic_name_transaction("/cron/run/" . $jobCode);
+            newrelic_name_transaction("/cron/run/" . $schedule->getJobCode());
         }
 
         parent::_runJob($scheduledTime, $currentTime, $jobConfig, $schedule, $groupId);
